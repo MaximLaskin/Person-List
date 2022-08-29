1200242f37d2e257c14a464905984d8ba2ca7eec
MaximLaskin <maxlaskin@mail.ru>
2022-08-30 00:01:21 +0300

    Initial Commit

---
+++ Person List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Person List
+//
+//  Created by Swift on 30.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
